93b818ba1a52ab97ec90d19b191d08d8f29fe5b4
luzhiqiang <listener_lzq@163.com>
2017-09-21 11:38:29 +0800

    init nodejs project && add .gitignore && finish base code

---
+++ index.js
@@ -0,0 +1,9 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const baseUrl = 'http://www.gaokaopai.com/daxue-0-0-0-0-0-0-0.html'
+const start = () => {
+  console.log('start fetch...' + baseUrl);
+}
+
+start()
